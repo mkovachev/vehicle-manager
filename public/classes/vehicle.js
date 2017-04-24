af5cc70f105f8e7980ee07c65874f1952b846d61
rmehmed <redzhebmehmed@abv.bg>
2017-04-24 18:21:20 +0300

    Started working on classes.

---
+++ public/classes/vehicle.js
@@ -0,0 +1,9 @@
+export class Vehicle {
+    constructor(brand, model, year, license, km) {     
+        this._brand = brand;
+        this._model = model;
+        this._year = year;
+        this._license = license;
+        this._km = km;
+    }
+}
